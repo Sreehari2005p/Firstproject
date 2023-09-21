4e6ee966d2d42d54fbf61c00cfa9e26b0d3ffb68
Sreehari A <Sreehari2005p@gmail.com>
2023-09-21 13:26:53 +0400

    tkinter2

--- PyProjects/PyGlobal/Tkinterlib.py
+++ PyProjects/PyGlobal/Tkinterlib.py
@@ -1,5 +1,6 @@
 import tkinter
 root=tkinter.Tk()
+root.title("Registration Form")
 
 name_label=tkinter.Label(root,text="Enter name")
 name_label.pack()
@@ -10,5 +11,13 @@ Email_label=tkinter.Label(root,text="Enter Email")
 Email_label.pack()
 Email_textbox=tkinter.Entry(root)
 Email_textbox.pack()
+
+Phone_label=tkinter.Label(root,text="Enter Phone Number")
+Phone_label.pack()
+Phone_textbox=tkinter.Entry(root)
+Phone_textbox.pack()
+
+submit_button=tkinter.Button(root,text="Submit")
+submit_button.pack()
 
 root.mainloop()
